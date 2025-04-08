8ca64a716ca6426199336404b7527c878d2c3fae
Nour Sherif Ali <Nour.98248@cs.mti.edu.eg>
2025-04-08 03:46:34 +0200

    commit2
    push auth firebase

--- src/app/login/login/login.component.ts
+++ src/app/login/login/login.component.ts
@@ -1,26 +1,23 @@
-import { Component, inject } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { AuthonService } from './../../services/authon.service';
-import { jwtDecode } from 'jwt-decode';  // matensash te3melha download men el cli npm i jwt-decode
+
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
-import { auth } from '../../firebase'; // أو حسب المسار المناسب
 import { signInWithEmailAndPassword } from 'firebase/auth';
+import { auth } from './../../firebase';
 
 @Component({
   selector: 'app-login',
-  imports: [ReactiveFormsModule, CommonModule, RouterLink],
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule, RouterLink],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
-
   isLoading: boolean = false;
   isErrorMsg: boolean = false;
+  errorMessage: string = '';
 
-  _AuthonService = inject(AuthonService);
-  _router = inject(Router);
-  
   signUpform = new FormGroup({
     email: new FormControl(null, [Validators.required, Validators.email]),
     password: new FormControl(null, [
@@ -29,62 +26,24 @@ export class LoginComponent {
     ]),
   });
 
-  login(form: any) {
-    console.log(form);
-    if (form.valid) {
-      this.isLoading = true;
-      this._AuthonService.signIn(form.value).subscribe({
-        next: (resp) => {
-          console.log(resp);
-          this.isErrorMsg = false;
-          this.isLoading = false;
-          localStorage.setItem('token', resp.token);
-          let decodedToken: any = jwtDecode(resp.token);
-          this._AuthonService.userName.next(decodedToken.name);
-          console.log(decodedToken);
-          this._AuthonService.isLogin.next(true);
-          this._router.navigate(['/home']);
-        },
-        error: (err) => {
-          console.log(err);
-          this.isLoading = false;
-          this.isErrorMsg = false;
-        },
-        complete: () => {},
-      });
-    } else {
-      console.log('form Invalid');
-    }
-  };
-  
-  getData(form: any) {
-    if (form.valid) {
+  constructor(private router: Router) {}
 
-      this.isLoading = true;
-      this._AuthonService.signIn(form.value).subscribe({
-        next: (resp) => {
-          console.log(resp);
-          this.isErrorMsg = false;
-          
-          localStorage.setItem('token',resp.token);
-          let decodedToken: any = jwtDecode(resp.token) ; //2e3mel7a import {jwtDecode} from 'jwt-decode'; 
-          this._AuthonService.userName.next(decodedToken.name);
-          console.log(decodedToken);
+  login() {
+    const email = this.signUpform.value.email;
+    const password = this.signUpform.value.password;
 
-          this._AuthonService.isLogin.next(true); // mayenfa3sh ne3melo '=' hayegblk error fa benesta5dem .next() we benekteb gowa el parameter TRUE, we next haye3mel ta8ayor fe observable
-          
-          
-          this._router.navigate(['/home'])
-        },
-        error: (err) => {
-          console.log(err);
-          this.isErrorMsg = true;
-          this.isLoading = false;
-        },
-        complete: () => {},
+    if (!email || !password) return;
+
+    this.isLoading = true;
+    signInWithEmailAndPassword(auth, email, password)
+      .then(userCredential => {
+        this.isLoading = false;
+        this.router.navigate(['/home']);
+      })
+      .catch(error => {
+        this.isLoading = false;
+        this.isErrorMsg = true;
+        this.errorMessage = error.message;
       });
-    } else {
-      console.log('form Invalid');
-    }
   }
 }
